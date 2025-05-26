5b54422947266d6e9a41a0a9d01232062b852e2d
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-05-26 13:28:54 +0700

    feat: create permission table

---
+++ src/common/models/permissions.model.ts
@@ -0,0 +1,42 @@
+import { BelongsTo, Column, DataType, Default, ForeignKey, Model, PrimaryKey, Table } from "sequelize-typescript";
+import { User } from "./users.model";
+
+@Table({ tableName: "permissions", timestamps: true })
+export class Permission extends Model<Permission> {
+  @PrimaryKey
+  @Default(DataType.UUIDV4)
+  @Column({ type: DataType.UUID, allowNull: false })
+  uuid: string;
+
+  @ForeignKey(() => User)
+  @Column({ type: DataType.UUID, allowNull: false })
+  user_uuid: string;
+
+  @Column({
+    type: DataType.DATEONLY,
+    allowNull: false,
+  })
+  start_date: Date;
+
+  @Column({
+    type: DataType.DATEONLY,
+    allowNull: false,
+  })
+  end_date: Date;
+
+  @Column({
+    type: DataType.TEXT,
+    allowNull: false,
+  })
+  reason: string;
+
+  @Column({
+    type: DataType.ENUM('pending', 'approved', 'rejected'),
+    allowNull: false,
+    defaultValue: 'pending',
+  })
+  status: 'pending' | 'approved' | 'rejected';
+
+  @BelongsTo(() => User)
+  user: User;
+}
