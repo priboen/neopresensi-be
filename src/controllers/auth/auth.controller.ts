da5c52da6af124c238784525cf91998edfeda548
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-05-26 21:00:33 +0700

    feat: auth login and register route

---
+++ src/controllers/auth/auth.controller.ts
@@ -0,0 +1,40 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { LoginDTO } from 'src/common/dto/data/login.dto';
+import { AuthInvalidResponseDto, AuthSuccessResponseDto, DuplicateResponseDto, NotFoundResponseDto, RegisterSuccessResponseDto, ValidationErrorResponseDto } from 'src/common/dto/responses/auth/auth-response.dto';
+import { RegisterDTO } from 'src/common/dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/login-web')
+  @ApiOperation({ summary: 'Admin Login for Web', description: 'User with admin role send a request to the server for log in into Admin Dashboard' })
+  @ApiBody({ type: LoginDTO })
+  @ApiResponse({ type: AuthSuccessResponseDto, status: 201, description: 'Login successful' })
+  @ApiResponse({ type: NotFoundResponseDto, status: 204, description: 'Invalid username or password' })
+  @ApiResponse({ type: AuthInvalidResponseDto, status: 400, description: 'Invalid username or password' })
+  async loginWeb(@Body() signInData: LoginDTO): Promise<AuthSuccessResponseDto | NotFoundResponseDto | AuthInvalidResponseDto> {
+    return this.authService.loginWeb(signInData);
+  }
+
+  @Post('/login-android')
+  @ApiOperation({ summary: 'User Login for MobileApps', description: 'User with user role send a request to the server for log in into Mobile Apps' })
+  @ApiResponse({ type: AuthSuccessResponseDto, status: 201, description: 'Login successful' })
+  @ApiResponse({ type: NotFoundResponseDto, status: 204, description: 'Invalid username or password' })
+  @ApiResponse({ type: AuthInvalidResponseDto, status: 400, description: 'Invalid username or password' })
+  async loginAndroid(@Body() signInData: LoginDTO): Promise<AuthSuccessResponseDto | NotFoundResponseDto | AuthInvalidResponseDto> {
+    return this.authService.loginAndroid(signInData);
+  }
+
+  @Post('/register')
+  @ApiOperation({ summary: 'Register', description: 'User send a request to the server for sign up into Mobile Apps' })
+  @ApiResponse({ type: RegisterSuccessResponseDto, status: 201, description: 'Registered successfully, please login' })
+  @ApiResponse({ type: ValidationErrorResponseDto, status: 400, description: 'Bad Request' })
+  @ApiResponse({ type: DuplicateResponseDto, status: 409, description: 'Username or Email already registered' })
+  @ApiBody({ type: RegisterDTO })
+  async signUp(@Body() registerData: RegisterDTO) {
+    return this.authService.register(registerData);
+  }
+}
---
+++ src/controllers/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { userProvider } from 'src/common/providers';
+
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {},
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, userProvider],
+  exports:[AuthService]
+})
+export class AuthModule {}
