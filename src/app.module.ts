cd5309c82c2402cb42e82cf3e93faf62dd4eb74b
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-05-26 21:00:14 +0700

    feat: global config for env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './controllers/auth/auth.module';
+import { MysqlModule } from './common/modules';
 
 @Module({
-  imports: [ConfigModule.forRoot(),],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }), 
+    MysqlModule, 
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
