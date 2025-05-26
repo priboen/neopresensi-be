aa273977248d48bd08975fd2459befa9730e0c66
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-05-26 20:56:44 +0700

    feat: config provider with configservice

--- src/common/providers/mysql.provider.ts
+++ src/common/providers/mysql.provider.ts
@@ -1,17 +1,22 @@
 import { Sequelize } from "sequelize-typescript";
 import { mysqlConfig } from "../configs";
 import { Attendance, Permission, User } from "../models";
+import { ConfigService } from "@nestjs/config";
 
 export const mysqlProvider = {
   provide: 'SEQUELIZE',
-  useFactory: async () => {
-    const sequelize = new Sequelize(mysqlConfig);
-    sequelize.addModels([
-      User,
-      Attendance,
-      Permission
-    ]);
-    await sequelize.sync();
+  useFactory: async (configService: ConfigService) => {
+    const sequelize = new Sequelize(mysqlConfig(configService));
+    sequelize.addModels([User, Attendance, Permission]);
+    try {
+      await sequelize.authenticate();
+      console.log('Database connection established successfully.');
+      await sequelize.sync();
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+      throw error;
+    }
     return sequelize;
-  }
+  },
+  inject: [ConfigService],
 };
