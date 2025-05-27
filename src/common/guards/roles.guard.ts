da167a118605f06608d86d0e9f85825c90c3bb68
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-05-27 22:14:19 +0700

    feat: change required role to user role

--- src/common/guards/roles.guard.ts
+++ src/common/guards/roles.guard.ts
@@ -1,21 +1,21 @@
-import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
-import { Reflector } from "@nestjs/core";
-import { Role } from "../enums/role.enum";
-import { ROLES_KEY } from "../decorators/roles.decorator";
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from '../enums/role.enum';
+import { ROLES_KEY } from '../decorators/roles.decorator';
 
 @Injectable()
-export class RolesGuard implements CanActivate{
+export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
-  canActivate(context: ExecutionContext): boolean  {
+  canActivate(context: ExecutionContext): boolean {
     const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
       context.getHandler(),
       context.getClass(),
-    ]); 
-    if(!requiredRoles) {
+    ]);
+    if (!requiredRoles) {
       return true;
     }
-    const {user} = context.switchToHttp().getRequest();
-    return requiredRoles.some(role => user?.role === role);
+    const { user } = context.switchToHttp().getRequest();
+    return requiredRoles.includes(user.role);
   }
 }
