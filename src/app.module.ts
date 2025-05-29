e6fd11f91d732035dfd07132128d6591a4134376
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-05-29 15:17:49 +0700

    feat:  import user module into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,17 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './controllers/auth/auth.module';
 import { MysqlModule } from './common/modules';
+import { UsersModule } from './controllers/users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
-    }), 
-    MysqlModule, 
-    AuthModule
+    }),
+    MysqlModule,
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
