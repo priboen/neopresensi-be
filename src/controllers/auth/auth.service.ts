c53870511894d785cbeb9794b87111e77edc65c0
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-05-29 15:16:35 +0700

    refactor: delete unused import, code and fix code indent

--- src/controllers/auth/auth.service.ts
+++ src/controllers/auth/auth.service.ts
@@ -1,5 +1,9 @@
 import * as bcrypt from 'bcrypt';
-import { Inject, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { LoginDTO, RegisterDTO, ResponseDto } from 'src/common/dto';
 import { User } from 'src/common/models';
@@ -10,65 +14,12 @@ export class AuthService {
   constructor(
     @Inject('USER_REPOSITORY') private readonly userRepository: typeof User,
     private readonly jwtService: JwtService,
-  ){}
+  ) {}
 
   private removePassword(user: User) {
     const { password, ...userWithoutPassword } = user.toJSON();
     return userWithoutPassword;
   }
-
-  // private async validateLogin(loginData: LoginDTO, requiredRole: 'admin' | 'guru'): Promise<ResponseDto<{ token: string }>> {
-  //   try {
-  //     const user = await this.userRepository.findOne({
-  //       where: { username: loginData.username },
-  //     });
-  //     if (!user) {
-  //       return new ResponseDto<{ token: string }>({
-  //         statusCode: 401,
-  //         message: 'Invalid username or password',
-  //       });
-  //     }
-  //     const userData = user.toJSON();
-  //     const isPasswordValid = await bcrypt.compare(
-  //       loginData.password,
-  //       userData.password,
-  //     );
-  //     if (!isPasswordValid) {
-  //       return new ResponseDto<{ token: string }>({
-  //         statusCode: 401,
-  //         message: 'Invalid username or password',
-  //       });
-  //     }
-  //     if (userData.role !== requiredRole) {
-  //       const errorMessage = requiredRole === 'admin'
-  //         ? 'Web login is restricted to admin users'
-  //         : 'Android login is restricted to teacher role';
-  //       throw new UnauthorizedException(errorMessage);
-  //     }
-  //     const token = await this.jwtService.signAsync({ uuid: userData.uuid, role: userData.role });
-  //     return new ResponseDto<{ token: string }>({
-  //       statusCode: 200,
-  //       message: 'Login successful',
-  //       data: { token },
-  //     });
-  //   } catch (error) {
-  //     if (error instanceof UnauthorizedException) {
-  //       return new ResponseDto<{ token: string }>({
-  //         statusCode: 401,
-  //         message: error.message,
-  //       });
-  //     }
-  //     throw new InternalServerErrorException('An error occurred during login', error.message);
-  //   }
-  // }
-
-  // async loginWeb(loginData: LoginDTO): Promise<ResponseDto<{ token: string }>> {
-  //   return this.validateLogin(loginData, 'admin');
-  // }
-
-  // async loginAndroid(loginData: LoginDTO): Promise<ResponseDto<{ token: string }>> {
-  //   return this.validateLogin(loginData, 'guru');
-  // }
 
   async login(loginData: LoginDTO): Promise<ResponseDto<{ token: string }>> {
     try {
@@ -92,14 +43,20 @@ export class AuthService {
           message: 'Invalid username or password',
         });
       }
-      const token = await this.jwtService.signAsync({ uuid: userData.uuid, role: userData.role });
+      const token = await this.jwtService.signAsync({
+        uuid: userData.uuid,
+        role: userData.role,
+      });
       return new ResponseDto<{ token: string }>({
         statusCode: 200,
         message: 'Login successful',
         data: { token },
       });
     } catch (error) {
-      throw new InternalServerErrorException('An error occurred during login', error.message);
+      throw new InternalServerErrorException(
+        'An error occurred during login',
+        error.message,
+      );
     }
   }
 
@@ -109,10 +66,11 @@ export class AuthService {
       const existingUser = await this.userRepository.findOne({
         where: {
           [Op.or]: [
-            { username: registerData.username }, 
-            { email: registerData.email }],
+            { username: registerData.username },
+            { email: registerData.email },
+          ],
         },
-      })
+      });
       if (existingUser) {
         return new ResponseDto<User>({
           statusCode: 409,
