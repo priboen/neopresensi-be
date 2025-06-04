9a13a38f830febf4329eb7f16138e49a05e79266
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-06-04 22:38:38 +0700

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,12 @@ config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true,
+  })
   app.setGlobalPrefix('api', {
     exclude: [
       {
