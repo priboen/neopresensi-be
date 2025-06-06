eb34561a11bb2318037d81fcab78fa96c00476f1
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-06-06 21:26:33 +0700

    feat: add optional

--- src/common/dto/data/user-update.dto.ts
+++ src/common/dto/data/user-update.dto.ts
@@ -1,19 +1,27 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 import { IsEmail, IsEnum, IsOptional, IsString } from 'class-validator';
 
 export class UserUpdateDto {
   @ApiPropertyOptional()
   @IsOptional()
   @IsString()
+  @Transform(({ value }) => (value === '' ? undefined : value))
   name?: string;
 
   @ApiPropertyOptional()
   @IsOptional()
   @IsString()
+  @Transform(({ value }) => (value === '' ? undefined : value))
   username?: string;
 
   @ApiPropertyOptional()
   @IsOptional()
   @IsEmail()
+  @Transform(({ value }) => (value === '' ? undefined : value))
   email?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  photo_url?: string;
 }
