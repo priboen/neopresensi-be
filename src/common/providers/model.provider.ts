080e09b31731c36a7be9b626300769c68a41237c
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-06-14 22:44:07 +0700

    feat: create provider

--- src/common/providers/model.provider.ts
+++ src/common/providers/model.provider.ts
@@ -1,20 +1,89 @@
-import { Attendance, Permission, User } from "../models";
+import {
+  Attendance,
+  CCTVConfig,
+  CCTVSchedule,
+  Classes,
+  ClassGroup,
+  Meeting,
+  MeetingAttendance,
+  Permission,
+  Schedule,
+  Subject,
+  TeacherAssignment,
+  User,
+} from '../models';
 
 export const userProvider = {
   provide: 'USER_REPOSITORY',
-  useValue: User
+  useValue: User,
 };
+
+export const classGroupProvider = {
+  provide: 'CLASS_GROUP_REPOSITORY',
+  useValue: ClassGroup,
+};
+
+export const classProvider = {
+  provide: 'CLASS_REPOSITORY',
+  useValue: Classes,
+};
+
+export const subjectProvider = {
+  provide: 'SUBJECT_REPOSITORY',
+  useValue: Subject,
+};
+
+export const teacherAssignmentProvider = {
+  provide: 'TEACHER_ASSIGNMENT_REPOSITORY',
+  useValue: TeacherAssignment,
+};
+
+export const scheduleProvider = {
+  provide: 'SCHEDULE_REPOSITORY',
+  useValue: Schedule,
+};
+
+export const cctvConfigProvider = {
+  provide: 'CCTV_CONFIG_REPOSITORY',
+  useValue: CCTVConfig,
+};
+
+export const cctvScheduleProvider = {
+  provide: 'CCTV_SCHEDULE_REPOSITORY',
+  useValue: CCTVSchedule,
+};
+
 export const attendanceProvider = {
   provide: 'ATTENDANCE_REPOSITORY',
-  useValue: Attendance
+  useValue: Attendance,
 };
-export const PermissionProvider = {
+
+export const meetingProvider = {
+  provide: 'MEETING_REPOSITORY',
+  useValue: Meeting,
+};
+
+export const meetingAttendanceProvider = {
+  provide: 'MEETING_ATTENDANCE_REPOSITORY',
+  useValue: MeetingAttendance,
+};
+
+export const permissionProvider = {
   provide: 'PERMISSION_REPOSITORY',
-  useValue: Permission
+  useValue: Permission,
 };
 
 export const modelProviders = [
   userProvider,
+  classGroupProvider,
+  classProvider,
+  subjectProvider,
+  teacherAssignmentProvider,
+  scheduleProvider,
+  cctvConfigProvider,
+  cctvScheduleProvider,
   attendanceProvider,
-  PermissionProvider
+  meetingProvider,
+  meetingAttendanceProvider,
+  permissionProvider,
 ];
