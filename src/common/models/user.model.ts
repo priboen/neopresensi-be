f0c1bb9d40b8a9befe97dc18953732744f3d94e8
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-06-14 22:43:10 +0700

    feat: add relation and refactor role guru to teacher

--- src/common/models/user.model.ts
+++ src/common/models/user.model.ts
@@ -2,12 +2,14 @@ import {
   Column,
   DataType,
   Default,
-  HasMany,
-  Model,
   PrimaryKey,
   Table,
+  HasMany,
+  Model,
 } from 'sequelize-typescript';
 import { Attendance } from './attendances.model';
+import { TeacherAssignment } from './teacher-assignments.model';
+import { MeetingAttendance } from './meeting-attendances.model';
 import { Permission } from './permissions.model';
 
 @Table({ tableName: 'users', timestamps: true })
@@ -20,34 +22,36 @@ export class User extends Model {
   @Column({ allowNull: false })
   name: string;
 
-  @Column({ unique: true })
+  @Column({ unique: true, allowNull: false })
   username: string;
 
-  @Column({ unique: true })
+  @Column({ unique: true, allowNull: false })
   email: string;
 
-  @Column
+  @Column({ allowNull: false })
   password: string;
-
-  @Column({
-    type: DataType.ENUM('guru', 'admin'),
-    allowNull: false,
-    defaultValue: 'guru',
-  })
-  role: 'guru' | 'admin';
 
   @Column({ type: DataType.TEXT, allowNull: true })
   face_embedding: string | null;
 
   @Column({
-    type: DataType.STRING,
-    allowNull: true,
-    comment: 'URL to uploaded file (image/pdf)',
+    type: DataType.ENUM('admin', 'teacher'),
+    allowNull: false,
+    defaultValue: 'teacher',
   })
+  role: 'admin' | 'teacher';
+
+  @Column({ type: DataType.STRING, allowNull: true })
   photo_url: string | null;
 
   @HasMany(() => Attendance, { onDelete: 'CASCADE' })
   attendances: Attendance[];
+
+  @HasMany(() => TeacherAssignment, { onDelete: 'CASCADE' })
+  teacherAssignments: TeacherAssignment[];
+
+  @HasMany(() => MeetingAttendance, { onDelete: 'CASCADE' })
+  meetingAttendances: MeetingAttendance[];
 
   @HasMany(() => Permission, { onDelete: 'CASCADE' })
   permissions: Permission[];
