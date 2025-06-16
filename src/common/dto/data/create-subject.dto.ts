7ee76ec3407976272511522aae305c61fb7ff88a
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-06-16 21:04:08 +0700

    fix: add is string and is not empty validator

--- src/common/dto/data/create-subject.dto.ts
+++ src/common/dto/data/create-subject.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class CreateSubjectDto {
   @ApiProperty({
     example: "Mathematics",
     description: "The name of the subject",
   })
+  @IsString()
+  @IsNotEmpty()
   name: string;
 }
